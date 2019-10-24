e70eeee487d03e68429980b9ef17ef7488b62c7c
student <student@eda-bc-03.localdomain>
2019-10-25 11:40:03 +1300

    fix student profile route

--- routes/students.js
+++ routes/students.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
+const studentData = require('../studentData.json')
 
-const studentData = JSON.parse(fs.readFileSync("studentData.json"));
 
 router.get('/', (req, res) => {
     const studentProfiles = 'partials/index' //this is a link to a hbs partial called index
@@ -11,20 +10,25 @@ router.get('/', (req, res) => {
         groupType: Object.keys(studentData),
         arrayOfGroup: studentData.students
     }                                     //JSON data taken from studentData.json, contains all student info
-    console.log(studentsInfo.arrayOfGroup)
-    console.log(studentsInfo.groupType)
     res.render(studentProfiles, studentsInfo) 
     //render the studentProfiles hbs content, and fill in the template with data from students
 })
 
-// router.get('/:name', (req, res) => {
-//     const {name} = req.params
-//     const profile = 'partials/profile' // this is a link to a hbs partial called profile
-//     const studentInfo = {
-//         //access specific student info from the studentData.json file
-//     }
-//     res.render(profile, studentInfo)
-// })
+router.get('/:name', (req, res) => {
+   
+    const {name} = req.params
+    const profile = 'partials/profile' // this is a link to a hbs partial called profile
+    const studentInfo = studentData.students.find(function(element) {
+            return element.name == name
+        })
+    
+        console.log('name is :', name)
+    // console.log(studentInfo)
+    console.log(studentData.students)
+      
+   
+    res.render(profile, studentInfo)
+})
 
 
 module.exports = router
