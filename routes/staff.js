e1abc647bf71d1306cc8ed8b53eba1245c83bf88
student <student@eda-bc-03.localdomain>
2019-10-25 12:27:06 +1300

    add comment post route for students

--- routes/staff.js
+++ routes/staff.js
@@ -3,31 +3,22 @@ const router = express.Router()
 const staffData = require('../staffData.json')
 
 
-router.get('/', (req, res) => {
-    const staffProfiles = 'partials/index' //this is a link to a hbs partial called index
-    
+router.get('/', (req, res) => {    
     const staffsInfo = {
         groupType: Object.keys(staffData),
         arrayOfGroup: staffData.staff
     }                                     //JSON data taken from staffData.json, contains all staff info
-    res.render(staffProfiles, staffsInfo) 
+    res.render('index', staffsInfo) 
     //render the staffProfiles hbs content, and fill in the template with data from staffs
 })
 
 router.get('/:name', (req, res) => {
    
     const {name} = req.params
-    const profile = 'partials/profile' // this is a link to a hbs partial called profile
     const staffInfo = staffData.staffs.find(function(element) {
             return element.name == name
-        })
-    
-        console.log('name is :', name)
-    // console.log(staffInfo)
-    console.log(staffData.staffs)
-      
-   
-    res.render(profile, staffInfo)
+        })   
+    res.render('profile', staffInfo)
 })
 
 
--- routes/students.js
+++ routes/students.js
@@ -1,34 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const studentData = require('../studentData.json')
+const fs = require('fs')
 
 
-router.get('/', (req, res) => {
-    const studentProfiles = 'partials/index' //this is a link to a hbs partial called index
-    
+router.get('/', (req, res) => {   
     const studentsInfo = {
         groupType: Object.keys(studentData),
         arrayOfGroup: studentData.students
     }                                     //JSON data taken from studentData.json, contains all student info
-    res.render(studentProfiles, studentsInfo) 
+    res.render('index', studentsInfo) 
     //render the studentProfiles hbs content, and fill in the template with data from students
 })
 
 router.get('/:name', (req, res) => {
    
     const {name} = req.params
-    const profile = 'partials/profile' // this is a link to a hbs partial called profile
     const studentInfo = studentData.students.find(function(element) {
             return element.name == name
-        })
-    
-        console.log('name is :', name)
-    // console.log(studentInfo)
-    console.log(studentData.students)
-      
-   
-    res.render(profile, studentInfo)
+        })    
+    res.render('profile', studentInfo)
 })
 
+router.post('/:name', (req, res) => {
+    const {name} = req.params
+    const newCommentData = {
+        commenter: req.body.commenter,
+        comment: req.body.comment,
+        date: req.body.date
+    }
+    
+    studentData.students.push(newCommentData)
+
+    let edited_studentData = JSON.stringify(studentData,null,2);
+    fs.writeFileSync('studentData.json', edited_studentData);
+    
+    res.redirect('/student/' + name)
+
+})
 
 module.exports = router
