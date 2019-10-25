0ecad7427a27855efd0c22a63601baf1cb36db47
student <student@eda-bc-03.localdomain>
2019-10-25 13:48:04 +1300

    need to fix action path in addComment.hbs form

--- routes/staff.js
+++ routes/staff.js
@@ -30,16 +30,20 @@ router.post('/:name', (req, res) => {
     const newCommentData = {
         commenter: req.body.commenter,
         comment: req.body.comment,
-        date: req.body.date
+        date: new Date ()
+       
     }
+    const staffInfo = staffData.staff.find(function(element) {
+        return element.name == name
+    })   
     
-    staffData.staff.push(newCommentData)
+    staffInfo.comments.push(newCommentData)
 
     let edited_staffData = JSON.stringify(staffData,null,2);
 
     fs.writeFileSync('staffData.json', edited_staffData);
     
-    res.send('<h1>test</h1>')
+    res.send('/staff/'+name)
 
 })
 
