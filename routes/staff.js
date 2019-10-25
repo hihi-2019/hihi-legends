0b22e5d1dfc3cd3d1ab0317927a51f31039d13f7
student <student@eda-bc-03.localdomain>
2019-10-25 14:03:31 +1300

    fix staff comment functionality

--- routes/staff.js
+++ routes/staff.js
@@ -8,17 +8,22 @@ router.get('/', (req, res) => {
     const staffsInfo = {
         groupType: Object.keys(staffData),
         arrayOfGroup: staffData.staff
-    }                                     //JSON data taken from staffData.json, contains all staff info
+    }                                   
     res.render('index', staffsInfo) 
-    //render the staffProfiles hbs content, and fill in the template with data from staffs
+
 })
 
 router.get('/:name', (req, res) => {
    
     const {name} = req.params
+
     const staffInfo = staffData.staff.find(function(element) {
             return element.name == name
         })   
+
+    staffInfo.groupType='staff'
+   
+
     res.render('profile', staffInfo)
 })
 
@@ -36,14 +41,15 @@ router.post('/:name', (req, res) => {
     const staffInfo = staffData.staff.find(function(element) {
         return element.name == name
     })   
-    
+    staffInfo.groupType='staff'
     staffInfo.comments.push(newCommentData)
 
     let edited_staffData = JSON.stringify(staffData,null,2);
 
     fs.writeFileSync('staffData.json', edited_staffData);
     
-    res.send('/staff/'+name)
+    res.render('profile', staffInfo)
+    console.log(staffInfo)
 
 })
 
